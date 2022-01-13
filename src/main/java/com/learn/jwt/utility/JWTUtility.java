c790a50906385f2dfe05ea913edc52b263412427
Harmesh Choubisa <harmesh.choubisa2@ing.com>
2022-01-13 01:02:25 +0100

    JWT authorization and using filter to authorize all rest calls

--- src/main/java/com/learn/jwt/utility/JWTUtility.java
+++ src/main/java/com/learn/jwt/utility/JWTUtility.java
@@ -1,5 +1,6 @@
 package com.learn.jwt.utility;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +11,13 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JWTUtility implements Serializable {
 
     private static final long serialVersionUID = 234234523523L;
-    private static final long JWT_TOKEN__VALIDITY = 5 * 60 * 60;
+    private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -40,8 +42,62 @@ public class JWTUtility implements Serializable {
      */
     private String doGenerateToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN__VALIDITY * 1000))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS256, secretKey).compact();
+    }
+
+
+    /**
+     * retrieve the username from the token
+     * @param token
+     * @return
+     */
+    public String getUsernameFromToken(String token) {
+         return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    /**
+     * for retrieving any information from token we will need the secret key
+     * @param token
+     * @return
+     */
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
+    /**
+     * check if the token has expired
+     * @param token
+     * @return
+     */
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    /**
+     * retrieve expiration date from jwt token
+     * @param token
+     * @return
+     */
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    /**
+     * validate token
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
 }
